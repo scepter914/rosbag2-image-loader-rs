13a4181c6f6e4c3dfc7c68e15240f42d490edd9c
scepter914 <scepter914@gmail.com>
2022-01-24 01:22:14 +0900

    adopt example for changing library
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- examples/example.rs
+++ examples/example.rs
@@ -1,5 +1,7 @@
+use rosbag2_image_loader::loader::load_images_from_rosbag2;
+
 fn main() {
     let file_name: String =
         "data/rosbag/rosbag2_2022_01_09-13_49_29/rosbag2_2022_01_09-13_49_29_0.db3".to_string();
-    rosbag2_loader::load_rosbag2(file_name).unwrap();
+    let result = load_images_from_rosbag2(file_name).unwrap();
 }
